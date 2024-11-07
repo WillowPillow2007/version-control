4d8ae9c77fd47aba1b8c9aa9ae0acee684f7f342
WillowPillow <thinkinfocustowin@gmail.com>
2024-11-07 19:33:43 +1100

    menu

--- public/src/menu.js
+++ public/src/menu.js
@@ -1,13 +1,41 @@
 document.addEventListener("DOMContentLoaded", function() {
     const instructionButton = document.querySelector(".instruction-button");
     const instructionOverlay = document.querySelector("#instruction-overlay");
-    const closeButton = document.querySelector(".close-button");
+    const playButton = document.querySelector(".play-button");
+    const playOverlay = document.querySelector("#play-overlay");
+    const closeButtons = document.querySelectorAll(".close-button");
+    const menuContainer = document.querySelector(".menu-container");
+    let isMenuBlurred = false;
 
     instructionButton.addEventListener("click", function() {
-        instructionOverlay.classList.add("show");
+    instructionOverlay.classList.add("show");
+    blurMenu();
     });
 
+    playButton.addEventListener("click", function() {
+    playOverlay.classList.add("show");
+    blurMenu();
+    });
+
+    closeButtons.forEach(function(closeButton) {
     closeButton.addEventListener("click", function() {
         instructionOverlay.classList.remove("show");
+        playOverlay.classList.remove("show");
+        unblurMenu();
     });
+    });
+
+    function blurMenu() {
+    if (!isMenuBlurred) {
+        menuContainer.classList.add("blurred");
+        isMenuBlurred = true;
+    }
+    }
+
+    function unblurMenu() {
+    if (isMenuBlurred) {
+        menuContainer.classList.remove("blurred");
+        isMenuBlurred = false;
+    }
+    }
 });
